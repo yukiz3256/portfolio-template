42d68209875334aa9a68f1144f43b5d9ac12a863
yukiz3256 <73492666+yukiz3256@users.noreply.github.com>
2020-10-26 13:59:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Yuki Z"
+let userName = "yukiz3256";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
